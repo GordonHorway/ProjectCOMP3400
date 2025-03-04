f4a106974e617023f0748c20bddef7325c85e515
Gordon Horway <ghorway@cogeco.ca>
2025-03-04 13:57:27 -0500

    More Building...

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include "classes.hpp"
 #include "library.hpp"
 
-int main(){
+int main(int argc, char **argv){
+
+    try{
+    if(argc != 2){
+        throw invalid_argument("invalid number of arguments passed into the CLI!!\n");
+    }
 
     string R_id, R_name;
     string C_id, C_name, C_address, C_phone, C_R_id;
 
-    EnergyProvider energyProvider("0111", "LargeCorporation", {{"1000", "Ontario"}, {"1001", "Quebec"}, {"1002", "Alberta"}, {"1003", "Manitoba"}, {"1004", "Saskatchewan"}});
+    EnergyProvider energyProvider(argv[1], "0111", "LargeCorporation", {{"1000", "Ontario"}, {"1001", "Quebec"}, {"1002", "Alberta"}, {"1003", "Manitoba"}, {"1004", "Saskatchewan"}});
 
     // loop for menu in terminal
     while(true){
@@ -17,6 +22,10 @@ int main(){
         cin >> choice;
         clear();
         switch(choice){
+            case 0:
+                // read from file into data structures
+                energyProvider.readFile();
+            break;
             case 1:
                 case 11:
 
@@ -25,7 +34,8 @@ int main(){
 
                 break;
                 case 13:
-
+                    getField(C_id, customerID);
+                    energyProvider.viewCustomer(C_id);
                 break;
                 case 14:
 
@@ -59,5 +69,7 @@ int main(){
                 cout << "Not a valid choice!" << endl;
         }
     }
-
+    }catch(invalid_argument &e){
+        cerr << e.what();
+    }
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 clear
-g++ -std=c++20 main.cpp library.cpp -o main && ./main
+g++ -std=c++20 main.cpp library.cpp -o main
+./main $1
 echo "return value : "$?
