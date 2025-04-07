34a8ebbe1fff8a82c12cf3492aa94da0019e565a
Gordon Horway <ghorway@cogeco.ca>
2025-04-07 14:07:38 -0400

    No longer need these shell scripts if I have a makefile that does the same thing but better

--- dynamic_run.sh
+++
@@ -1,3 +0,0 @@
-clear
-g++ -std=c++20 -Wall -Wextra -Werror -pedantic -O3 -march=native -lsqlite3 main.cpp library.cpp -o main || rm main
-./main energy_prices.txt
--- static_run.sh
+++
@@ -1,3 +0,0 @@
-clear
-g++ -std=c++20 -Wall -Wextra -Werror -pedantic -O3 -march=native main.cpp library.cpp ./libsqlite3.a -o main || rm main
-./main energy_prices.txt
